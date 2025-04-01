60e5094a1ac9a98188b3c8ef9d86b3994a4e2886
Mariama Daff <daffmariam8@gmail.com>
2025-04-01 15:29:36 +0200

    Delete Mantorat/Integration_Continue directory

--- Mantorat/Integration_Continue/.history/fizzbuzz.test_20250401121158.js
+++
@@ -1,7 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert');
-const { fizzbuzz } = require('./fizzbuzz.js')
-
-test('fizzbuzz(3)', (t) => {
-  assert.strictEqual(fizzbuzz(3), "Fizz");
-});
